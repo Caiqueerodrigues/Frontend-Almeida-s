0c613a0f68c94473e6c2869799c5200831a81845
Caique Rodrigues <110926789+Caiqueerodrigues@users.noreply.github.com>
2025-05-11 19:46:27 -0300

    tempo timeout 15 seg

--- plugins/axios.js
+++ plugins/axios.js
@@ -10,7 +10,7 @@ export default defineNuxtPlugin((nuxtApp) => {
 
     const axiosInstance = axios.create({
         baseURL: baseURL,
-        timeout: 5000, 
+        timeout: 15000, 
     });
 
     axiosInstance.interceptors.request.use(config => {
