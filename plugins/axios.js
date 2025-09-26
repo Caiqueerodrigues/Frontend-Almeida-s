cea483c2edbc129b5f9e1a66e42fdf6ec820ad93
Caíque Rodrigues <caiquerodriguesalmeida@gmail.com>
2025-09-26 17:02:41 -0300

    aumentando tempo do timeout

--- plugins/axios.js
+++ plugins/axios.js
@@ -51,7 +51,7 @@ export default defineNuxtPlugin((nuxtApp) => {
             removeToken();
             setTimeout(() => {
                 router.push('/login');
-            }, 1000 * 5);
+            }, 1000 * 10);
         }
         
         return Promise.reject(error);
